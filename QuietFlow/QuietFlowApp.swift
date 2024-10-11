8d0a7df12ec1cac8356117909b7bc6f85e51de65
Mateo Mercado <matemerc@gmail.com>
2024-10-11 18:17:30 -0400

    Initial Commit

---
+++ QuietFlow/QuietFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuietFlowApp.swift
+//  QuietFlow
+//
+//  Created by Mateo Mercado on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct QuietFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
